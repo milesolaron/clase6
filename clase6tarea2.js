fc02d102557fa886f30f24519ff262f067963dbe
milesolaron <127025206+milesolaron@users.noreply.github.com>
2023-06-15 09:10:22 -0300

    Update clase6tarea2.js
    Realicé las modificaciones que Ana sugirió. 
    Incorporé comentarios para que mi código sea más fácil de comprender. Mejoré los nombers de las variables y realicé cálculos divididos en funciones mediante la iteración de arrays y algoritmos.

--- clase6tarea2.js
+++ clase6tarea2.js
@@ -1,58 +1,101 @@
-let inputs = [];
+let $salariosAnuales = [];
 
-function agregarInput() {
-    const input = document.createElement("input");
-    input.type = "number";
-    const label = document.createElement("label");
-    label.textContent = "Salario anual: ";
+//Función que le da la funcionalidad al botón de "Agregar" para que el usuario tipee los salarios.
+function agregarSalariosAnuales() {
+    const $salarioAnual = document.createElement("input");
+    $salarioAnual.type = "number";
+    const $labelSalarioAnual = document.createElement("label");
+    $labelSalarioAnual.textContent = "Salario anual: ";
     
-    document.getElementById("container").appendChild(label);
-    document.getElementById("container").appendChild(input);
+    document.getElementById("container").appendChild($labelSalarioAnual);
+    document.getElementById("container").appendChild($salarioAnual);
 
-    inputs.push(input);
+    $salariosAnuales.push($salarioAnual);
 }
 
-function quitarInput() {
-    if (inputs.length > 0) {
-        let input = inputs.pop();
-        input.parentNode.removeChild(input);
-        let label = input.previousSibling;
-        label.parentNode.removeChild(label);
+//Función que le da la funcionalidad al botón de "Agregar" para que el usuario elimine los salarios que requiera.
+function quitarSalariosAnuales() {
+    if ($salariosAnuales.length > 0) {
+        let $salarioAnual = $salariosAnuales.pop();
+        let $labelSalarioAnual = $salarioAnual.previousElementSibling;
+
+        $salarioAnual.parentNode.removeChild($salarioAnual);
+        $labelSalarioAnual.parentNode.removeChild($labelSalarioAnual);
     }
 }
 
-function calcularSalarios() {
-    let salarios = [];
+//Función obtiene los valores de los salarios que cargó el usuario, verificando que el número sea válido (es decir, que no esté vacío el input y que se trate de un número)
+function obtenerSalarios() {
+    const salarios = [];
 
-    inputs.forEach(function (input) {
-        let salario = parseInt(input.value);
+    $salariosAnuales.forEach(function ($salarioAnual) {
+        const salario = parseInt($salarioAnual.value);
         if (!isNaN(salario) && salario !== '') {
             salarios.push(salario);
         }
     });
 
-if (salarios.length > 0) {
-    const mayorSalario = Math.max(...salarios);
-    const menorSalario = Math.min(...salarios);
-    const salarioPromedioAnual = calcularPromedio(salarios);
-    const salarioPromedioMensual = salarioPromedioAnual / 12;
-
-    document.getElementById("mayor-salario").textContent = mayorSalario;
-    document.getElementById("menor-salario").textContent = menorSalario;
-    document.getElementById("promedio-anual").textContent = salarioPromedioAnual;
-    document.getElementById("promedio-mensual").textContent = salarioPromedioMensual;
-} else {
-    document.getElementById("mayorSalario").textContent = '';
-    document.getElementById("menorSalario").textContent = '';
-    document.getElementById("promedioSalarioAnual").textContent = '';
-    document.getElementById("promedioSalarioMensual").textContent = '';
-}
+    return salarios;
 }
 
-function calcularPromedio(salarios) {
-    const total = salarios.reduce(function (suma, salario) {
-        return (suma + salario);
-    }, 0);
-    return total / salarios.length;
+//Funciones que realizan los cálculos de mayor, menor y promedios mediante iterar arrays y algoritmos
+function calcularMayorSalario(salarios) {
+    let mayorSalario = salarios[0];
+
+    for (let i = 1; i < salarios.length; i++) {
+        if (salarios[i] > mayorSalario) {
+            mayorSalario = salarios[i];
+        }
+    }
+    return mayorSalario;
 }
 
+function calcularMenorSalario(salarios) {
+    let menorSalario = salarios[0];
+
+    for (let i = 1; i < salarios.length; i++) {
+        if (salarios[i] < menorSalario) {
+            menorSalario = salarios[i];
+        }
+    }
+    return menorSalario;
+}
+
+function calcularPromedioAnual(salarios) {
+    let totalSalarios = 0;
+
+    for (let i = 0; i < salarios.length; i++) {
+        totalSalarios += salarios[i];
+    }
+
+    return totalSalarios / salarios.length;
+}
+
+function calcularPromedioMensual(salarioPromedioAnual) {
+    return (salarioPromedioAnual / 12);
+}
+
+//Función que interactúa con el DOM para mostrar los resultados de los cálculos de las funciones anteriores en el HTML.
+function colocarResultados() {
+    const salarios = obtenerSalarios();
+
+    if (salarios.length > 0) {
+        const mayorSalario = calcularMayorSalario(salarios);
+        const menorSalario = calcularMenorSalario(salarios);
+        const salarioPromedioAnual = calcularPromedioAnual(salarios);
+        const salarioPromedioMensual = calcularPromedioMensual(salarioPromedioAnual);
+
+        document.getElementById("mayor-salario").textContent = mayorSalario;
+        document.getElementById("menor-salario").textContent = menorSalario;
+        document.getElementById("promedio-anual").textContent = salarioPromedioAnual;
+        document.getElementById("promedio-mensual").textContent = salarioPromedioMensual;
+    } else {
+        document.getElementById("mayor-salario").textContent = '';
+        document.getElementById("menor-salario").textContent = '';
+        document.getElementById("promedio-anual").textContent = '';
+        document.getElementById("promedio-mensual").textContent = '';
+
+    }
+}
+
+colocarResultados();
